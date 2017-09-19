8adef193c7d96c4dbaef0d73faf5c5ef56942eeb
pmiskew <pmiskew@10.7.6.165>
2017-09-19 11:28:49 -0400

    Mac Committ

---
+++ src/MathExample.java
@@ -0,0 +1,26 @@
+
+/**
+ * This is a header for the class.  It will be in blue
+ * Headers for methods will be in blue as well
+ * @author pmiskew
+ *
+ */
+public class MathExample {
+
+	/**
+	 * This is my main method.  This is where the program 
+	 * starts running.  Every programming language needs a 
+	 * starting point.  Python for example starts at the first
+	 * non indented line of code. 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Math Example");
+		
+		
+	} //end main
+	
+	
+
+} // end class
