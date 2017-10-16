71afdd982ac95828cba3322c00bf65ca13d546be
pmiskew <pmiskew@10.7.6.199>
2017-10-16 09:37:03 -0400

    MAC Committ

---
+++ src/displaying_images/ImageExample.java
@@ -0,0 +1,35 @@
+package displaying_images;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class ImageExample {
+
+	
+	private JFrame frame = new JFrame("Image Example");
+
+	private BufferedImage img = null;
+	
+	public ImageExample() {
+		
+		frame.setLocation(100,100);
+	
+		try {
+		    img = ImageIO.read(new File("helium.jpg"));
+		} catch (IOException e) {
+			System.out.println("Not found");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ImageExample ie = new ImageExample();
+		
+	}
+
+}
